70c8bd233fe596dd54946d683d946ef3d9553a54
Ryan Morr <rm.morr@gmail.com>
2020-01-14 20:57:09 -0500

    Initial implementation

---
+++ src/attr.js
@@ -0,0 +1,6 @@
+export default function attr(element, name, value) {
+    if (typeof name === 'object') {
+        return Object.keys(name).forEach((key) => attr(element, key, name[key]));
+    }
+    element.setAttribute(name, value);
+}
---
+++ test/specs/attr.js
@@ -0,0 +1,30 @@
+import attr from '../../src/attr';
+
+describe('attr', () => {
+    let element;
+
+    beforeEach(() => {
+        element = document.createElement('div');
+        document.body.appendChild(element);
+    });
+
+    afterEach(() => {
+        document.body.removeChild(element);
+    });
+
+    it('should set an attribute', () => {
+        attr(element, 'foo', 'bar');
+
+        expect(element.getAttribute('foo')).to.equal('bar');
+    });
+
+    it('should set multiple attributes', () => {
+        attr(element, {
+            foo: 'bar',
+            baz: 'qux'
+        });
+
+        expect(element.getAttribute('foo')).to.equal('bar');
+        expect(element.getAttribute('baz')).to.equal('qux');
+    });
+});
